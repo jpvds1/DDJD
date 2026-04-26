279c165b08ff69a0c58f61fae735eb91f9487fa8
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-04-26 23:35:06 +0100

    perf(boost): use global transform

--- platformer-game/scripts/systems/boost_ring.gd
+++ platformer-game/scripts/systems/boost_ring.gd
@@ -1,19 +1,12 @@
 extends Area3D
 
 
-const BOOST = 5.0
-const NORMAL = Vector3(0, 1, 0)
+const BOOST = 5
 
 
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("player"):
 		return
-	
-	var direction = Vector3(0, 1, 0).rotated(Vector3(1, 0, 0), rotation.x) \
-		.rotated(Vector3(0, 1, 0), rotation.y) \
-		.rotated(Vector3(0, 0, 1), rotation.z) \
-		.normalized()
-	
-	print(body.position)
-	body.position += BOOST * direction
-	print(body.position)
+
+	# boost the player in the direction orthogonal to the ring
+	body.position += BOOST * global_transform.basis.y
