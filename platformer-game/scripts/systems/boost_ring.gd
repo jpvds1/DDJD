9babfe4b7a77925fac2f24674c4f06f4d84b915f
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-04-27 23:53:21 +0100

    feat(boost): export customizable variables

--- platformer-game/scripts/systems/boost_ring.gd
+++ platformer-game/scripts/systems/boost_ring.gd
@@ -1,7 +1,8 @@
 extends Area3D
 
 
-@export var boost = 12
+@export var boost: float = 12
+@export var bidirectional = false
 
 
 func _on_body_entered(body: Node3D) -> void:
@@ -11,6 +12,7 @@ func _on_body_entered(body: Node3D) -> void:
 	# detect if the player is entering the ring from the front or behind
 	var normal = global_transform.basis.y
 	var angle = body.velocity.angle_to(normal)
+	var multiplier = -1 if bidirectional and angle > PI / 2 else 1
 
 	# boost the player in the direction orthogonal to the ring
-	body.velocity += boost * normal * (1 if angle <= PI / 2 else -1)
+	body.velocity += boost * multiplier * normal
