6007917e724620bd91b5fedf7ace9cf6594b2d84
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-14 22:54:50 +0100

    feat(saw): make rotations per second customizable

---
+++ platformer-game/scripts/systems/saw.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+@export var rotations_per_second: float = 0.5
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.speed_scale = rotations_per_second
