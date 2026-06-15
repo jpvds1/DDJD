8ed103232707723858f0404ba7d3f865307e17f1
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-15 01:29:42 +0100

    feat(wrecking ball): customize swing speed

--- platformer-game/scripts/traps/wrecking_ball.gd
+++ platformer-game/scripts/traps/wrecking_ball.gd
@@ -1,17 +1,23 @@
 extends Node3D
 
-@export var max_angle: float = 30.0
-@export var spins_per_second: float = 2
-@export var swing: float = 0:
+
+@onready var spike_ball: MeshInstance3D = $Visuals/Icosphere
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@export var max_angle := 30.0
+@export var swings_per_second := 1.0
+@export var spins_per_second := 1.0
+@export var swing := 0.0:
 	set(value):
 		if value >= -1 && value <= 1:
 			swing = value
 
-@onready var spike_ball: MeshInstance3D = $Visuals/Icosphere
+var spin_speed := spins_per_second * 2 * PI
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.speed_scale = swings_per_second
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +26,4 @@ func _process(delta: float) -> void:
 	rotation_degrees.z = swing * max_angle
 	
 	# spin the spike ball
-	spike_ball.rotate_y(spins_per_second * delta)
+	spike_ball.rotate_y(spin_speed * delta)
