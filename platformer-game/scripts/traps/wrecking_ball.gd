b4cde7407732798bf5458a17462b8587aebd7961
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-15 16:46:41 +0100

    feat(wrecking ball): update animation

--- platformer-game/scripts/traps/wrecking_ball.gd
+++ platformer-game/scripts/traps/wrecking_ball.gd
@@ -1,27 +1,30 @@
 extends Node3D
 
-
-@onready var spike_ball: MeshInstance3D = $Visuals/Icosphere
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var wrecking_ball: Node3D = $Ball
+@onready var spike_ball: MeshInstance3D = $Ball/Visuals/Icosphere
 
 @export var max_angle := 30.0
 @export var swings_per_second := 1.0
-@export var spins_per_second := 1.0
 
-var spin_speed := spins_per_second * 2 * PI
-var swing := 0.0:
-	set(value):
-		swing = clamp(value, -1, 1)
+var spin_speed: float:
+	get:
+		return 0.7 * swings_per_second * 2 * PI # radians per second
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animation_player.speed_scale = swings_per_second
+	# set up the tweener
+	var tween := create_tween()
+	tween.set_trans(Tween.TRANS_SINE)
+	tween.set_loops() # loop infinitely
+	
+	# configure the animation keyframes
+	var swing_duration := 1.0 / swings_per_second # seconds
+	tween.tween_property(wrecking_ball, "rotation_degrees:z", max_angle, swing_duration)
+	tween.tween_property(wrecking_ball, "rotation_degrees:z", -max_angle, swing_duration)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# swing the wrecking ball around its pivot
-	rotation_degrees.z = swing * max_angle
-	
-	# spin the spike ball
+	# infinitely spin the spike ball
 	spike_ball.rotate_y(spin_speed * delta)
