f4fe588d04a649cfbc3afdc6e8f5755b95b7a4c9
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-15 01:16:26 +0100

    feat(wrecking ball): rotate spike ball

--- platformer-game/scripts/traps/wrecking_ball.gd
+++ platformer-game/scripts/traps/wrecking_ball.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
 @export var max_angle: float = 30.0
+@export var spins_per_second: float = 2
 @export var swing: float = 0:
 	set(value):
 		if value >= -1 && value <= 1:
 			swing = value
 
+@onready var spike_ball: MeshInstance3D = $Visuals/Icosphere
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +16,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# swing the wrecking ball around its pivot
 	rotation_degrees.z = swing * max_angle
+	
+	# spin the spike ball
+	spike_ball.rotate_y(spins_per_second * delta)
