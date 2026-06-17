160b68dd55f350563ce373d98b65ea61ab776f3b
João Silva <jpvds1@hotmail.com>
2026-06-17 17:03:18 +0100

    feat: connected level 2

--- platformer-game/scripts/ui/levels_menu.gd
+++ platformer-game/scripts/ui/levels_menu.gd
@@ -24,7 +24,12 @@ func _on_level_2_pressed() -> void:
 
 
 func _on_level_3_pressed() -> void:
-	pass # Replace with function body.
+	# Tira o foco de qualquer elemento de UI (como o próprio botão)
+	get_viewport().gui_release_focus()
+	
+	# call_deferred garante que a troca só ocorra após o clique terminar
+	var path = "res://scenes/levels/level_2.tscn"
+	Global.game_controller.change_3D_scene.call_deferred(path)
 
 
 func _on_level_4_pressed() -> void:
