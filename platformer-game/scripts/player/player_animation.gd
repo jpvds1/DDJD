06285af2fcc856b8b7f5fc5e39f480002c9c21e9
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-18 14:32:10 +0100

    feat(player): play slide animation in-game

--- platformer-game/scripts/player/player_animation.gd
+++ platformer-game/scripts/player/player_animation.gd
@@ -13,8 +13,9 @@ extends Node3D
 
 var _player: CharacterBody3D
 var _fall_blend := 0.0
+var _slide_blend := 0.0
 
-@export var blend_strength := 20.0
+@export var blend_strength := 15.0
 
 # ---------------------------------------------------------
 # Methods
@@ -32,20 +33,29 @@ func _ready() -> void:
 	_player.extra_jumps_changed.connect(_on_extra_jumps_changed)
 
 func _physics_process(delta: float) -> void:
+	var lerp_weight := blend_strength * delta
+
+	# compute the movement blending value
 	var max_sprint_speed: float = stats.sprint_speed.get_val()
 	var local_velocity := _player.global_transform.basis.inverse() * _player.velocity
-	var lerp_weight := blend_strength * delta
+	var movement_blend := Vector2(local_velocity.x, -local_velocity.z) / max_sprint_speed
 	
-	# compute the fall blending value
-	_fall_blend = lerp(
-		_fall_blend,
-		float(_player.is_on_floor() or _player.is_on_wall()),
+	# compute the slide blending value
+	_slide_blend = lerp(
+		_slide_blend,
+		float(_player.is_sliding),
 		lerp_weight
 	)
 
-	# compute the movement blending value
-	var movement_blend := Vector2(local_velocity.x, -local_velocity.z) / max_sprint_speed
-	
+	# compute the fall blending value
+	var is_airborne := !is_zero_approx(_player.velocity.y)
+	_fall_blend = lerp(
+		_fall_blend,
+		float(is_airborne),
+		lerp_weight
+	)
+
 	# update the blending values
-	animation_tree.set("parameters/Fall/blend_amount", _fall_blend)
 	animation_tree.set("parameters/Movement/blend_position", movement_blend)
+	animation_tree.set("parameters/Slide/blend_amount", _slide_blend)
+	animation_tree.set("parameters/Fall/blend_amount", _fall_blend)
