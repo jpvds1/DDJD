1e0e0d2a7e5f8b1bd32e65fba5954ffec859260e
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-20 16:28:58 +0100

    feat(ghost): use new player model

--- platformer-game/scripts/player/ghost_character.gd
+++ platformer-game/scripts/player/ghost_character.gd
@@ -4,26 +4,18 @@ var replay_data: Array = []
 var current_frame := 0
 var is_playing := false
 
-@onready var animation_player: AnimationPlayer = $Node3D/Dash/AnimationPlayer
-@onready var animation_tree: AnimationTree = $Node3D/Visuals/Body/AnimationTree
-@onready var visuals: Node3D = $Node3D/Visuals
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $Visuals/AnimationTree
+@onready var visuals: Node3D = $Visuals
+@onready var camera_3d: Camera3D = $CameraPivot/Camera3D
+
+# ---------------------------------------------------------
+# Methods
+# ---------------------------------------------------------
 
 func _ready() -> void:
-	_setup_wings()
 	_apply_ghost_material()
 
-func _setup_wings() -> void:
-	var skeleton := $Node3D/Visuals/Body/Armature/Skeleton3D as Skeleton3D
-	if skeleton == null:
-		return
-	var attachment := BoneAttachment3D.new()
-	attachment.bone_name = "mixamorig_Head"
-	skeleton.add_child(attachment)
-	attachment.bone_idx = skeleton.find_bone("mixamorig_Head")
-	var wings: Node3D = (load("res://assets/models/cosmetics/wings.glb") as PackedScene).instantiate()
-	wings.scale = Vector3(100, 100, 100)
-	wings.position = Vector3(0.07690239, 38.89086, -3.3907375)
-	attachment.add_child(wings)
 
 func _apply_ghost_material() -> void:
 	var mat := StandardMaterial3D.new()
@@ -32,11 +24,13 @@ func _apply_ghost_material() -> void:
 	mat.roughness = 0.5
 	_apply_material_recursive(self, mat)
 
+
 func _apply_material_recursive(node: Node, mat: StandardMaterial3D) -> void:
 	if node is MeshInstance3D:
 		(node as MeshInstance3D).material_override = mat
 	for child in node.get_children():
 		_apply_material_recursive(child, mat)
+
 
 func start_replay(data: Array) -> void:
 	if data.is_empty():
@@ -45,6 +39,7 @@ func start_replay(data: Array) -> void:
 	current_frame = 0
 	is_playing = true
 	visible = true
+
 
 func _physics_process(_delta: float) -> void:
 	if not is_playing or replay_data.is_empty():
