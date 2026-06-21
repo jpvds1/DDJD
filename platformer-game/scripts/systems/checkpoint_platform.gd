1e0ed52a341e7da56bf54735da49062e23649eff
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-21 16:25:44 +0100

    feat(checkpoint): add sound effect

--- platformer-game/scripts/systems/checkpoint_platform.gd
+++ platformer-game/scripts/systems/checkpoint_platform.gd
@@ -2,8 +2,9 @@ extends Node3D
 
 @onready var area: Area3D = $Area3D
 @onready var respawn_point: Marker3D = $Marker3D
+@onready var audio_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
-var activated := false
+var _activated := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,9 +12,9 @@ func _ready() -> void:
 	area.body_entered.connect(_on_area_3d_body_entered)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if activated:
+	if _activated or not body.is_in_group("player"):
 		return
-	
-	if body.is_in_group("player"):
-		body.request_checkpoint(respawn_point.global_position)
-		activated = true
+
+	_activated = true
+	body.request_checkpoint(respawn_point.global_position)
+	audio_player.play()
