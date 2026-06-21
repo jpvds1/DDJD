b93bdfb39c49be70a4c2bd8f19b6f2057f303602
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-21 22:29:53 +0100

    feat(spike trap): script new logic

--- platformer-game/scripts/systems/spikes.gd
+++ platformer-game/scripts/systems/spikes.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var retract_timer: Timer = $RetractTimer
+@onready var delay_timer: Timer = $DelayTimer
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not animation_player.is_playing():
 		animation_player.play("out")
-		retract_timer.start()
 
 
-func _on_retract_timer_timeout() -> void:
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "out":
+		delay_timer.start()
+
+
+func _on_delay_timer_timeout() -> void:
 	animation_player.play("hide")
