fcf982864983a0455094f5c19004ff80b21f6a9e
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-21 21:18:40 +0100

    feat(spikes): add model

--- platformer-game/scripts/systems/spikes.gd
+++ platformer-game/scripts/systems/spikes.gd
@@ -3,10 +3,12 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var retract_timer: Timer = $RetractTimer
 
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		animation_player.play("out")
 		retract_timer.start()
 
+
 func _on_retract_timer_timeout() -> void:
-	animation_player.play("hidden")
+	animation_player.play("hide")
