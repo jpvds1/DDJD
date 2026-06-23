f3f29742f699a1614b89b31284f232cb64989b87
Tomás Sucena Lopes <tomas21sucena@gmail.com>
2026-06-23 01:27:34 +0100

    feat(boost ring): always launch player

--- platformer-game/scripts/systems/boost_ring.gd
+++ platformer-game/scripts/systems/boost_ring.gd
@@ -4,7 +4,7 @@ extends Area3D
 
 @export var boost_speed: float = 20
 @export var bidirectional: bool = false
-@export var control_lock_duration: float = 0.5
+@export var control_lock_duration: float = 1.0
 
 
 func _on_body_entered(body: Node3D) -> void:
@@ -14,13 +14,9 @@ func _on_body_entered(body: Node3D) -> void:
 	var ring_forward := global_transform.basis.y
 	var is_going_forward = body.velocity.dot(ring_forward) >= 0.0
 
-	if not is_going_forward and not bidirectional:
-		return
-
 	# boost the player in the direction orthogonal to the ring
-	var launch_dir := ring_forward * (1 if is_going_forward else -1)
-	
-	body.position = position
+	var launch_dir := ring_forward * (1 if is_going_forward or not bidirectional else -1)
+	#body.position = position
 	body.apply_boost(launch_dir * boost_speed, control_lock_duration)
 	
 	# play the sound effect
